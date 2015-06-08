7293c24a9b3e14640076132cc5bbd19fb378e59c
Rog <rog@hatbat.net>
2015-06-08 17:40:24 +0100

    Text field handling in new transaction

--- PocketMoney/NewTransactionViewController.swift
+++ PocketMoney/NewTransactionViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class NewTransactionViewController: UITableViewController {
+public class NewTransactionViewController: UITableViewController {
 
     var account : Account?
     
@@ -18,22 +18,17 @@ class NewTransactionViewController: UITableViewController {
     
     var lastTransactionName : String?
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    override public func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        nameEdit.becomeFirstResponder()
     }
-
-    override func didReceiveMemoryWarning() {
+    override public func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    override func viewDidDisappear(animated: Bool) {
+    override public func viewDidDisappear(animated: Bool) {
         // Check that we are being dismissed from the top of the stack. Means back has been pressed and we should create a
         // new transaction
         if let description = self.nameEdit.text, ac = account, vcs = self.navigationController?.viewControllers as? [NSObject] {
@@ -81,59 +76,48 @@ class NewTransactionViewController: UITableViewController {
             }
         }
     }
-    // MARK: - Table view data source
 
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
+}
+extension NewTransactionViewController : UITextFieldDelegate {
+    
+    // MARK: UITExtViewDelegate
 
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
+    public func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if textField == nameEdit {
+            amountEdit.becomeFirstResponder()
+        }
         return true
     }
-    */
+    
+    public func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
+        if textField == amountEdit {
+            let existingString = amountEdit.text as NSString
+            let proposedString = existingString.stringByReplacingCharactersInRange(range, withString: string)
+            let components = proposedString.componentsSeparatedByString(".")
+            if count(components) > 2 {
+                return false
+            }
+            let pennies = components[1]
+            if count(pennies) > 2 {
+                return false
+            }
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
+        }
         return true
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    
+    public func textFieldShouldEndEditing(textField: UITextField) -> Bool {
+        if textField == nameEdit {
+            let string = nameEdit.text
+            if count(string) > 1 {
+                return true
+            }
+            return false
+            
+        }
+        return true
     }
-    */
+
+
 }
